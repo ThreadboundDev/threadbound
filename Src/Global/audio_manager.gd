b46ece332254caaa98c3f2322bf2a485b658f36e
ThreadboundDev <threadbounddev@gmail.com>
2026-06-25 07:45:48 -0400

    feature: add audio volume categories

--- Src/Global/audio_manager.gd
+++ Src/Global/audio_manager.gd
@@ -13,6 +13,9 @@ const MUSIC_BUS := &"Music"
 const SFX_BUS := &"SFX"
 const UI_BUS := &"UI"
 const AMBIENCE_BUS := &"Ambience"
+const AUDIO_SETTINGS_PATH := "user://audio_settings.cfg"
+const MIN_BUS_VOLUME_DB := -60.0
+const DEFAULT_BUS_VOLUME := 1.0
 
 @export var registry: Resource = DEFAULT_REGISTRY
 @export var sfx_pool_size := 16
@@ -27,6 +30,7 @@ var _music_player: AudioStreamPlayer
 func _ready() -> void:
 	randomize()
 	_ensure_runtime_buses()
+	load_audio_settings()
 	_build_pool(_sfx_players, sfx_pool_size, SFX_BUS, "SFXPlayer")
 	_build_pool(_ui_players, ui_pool_size, UI_BUS, "UIPlayer")
 	_music_player = _create_player(MUSIC_BUS, "MusicPlayer")
@@ -96,6 +100,102 @@ func play_music(sound_name: StringName, volume_offset_db := 0.0) -> AudioStreamP
 func stop_music() -> void:
 	if _music_player:
 		_music_player.stop()
+
+func get_volume_categories() -> Array[StringName]:
+	return [&"master", &"music", &"sfx", &"ui", &"ambience"]
+
+func set_category_volume(category: StringName, volume: float, save_settings := true) -> void:
+	var bus := _category_to_bus(category)
+	if bus == &"":
+		push_warning("AudioManager could not find volume category '%s'." % String(category))
+		return
+
+	set_bus_volume_linear(bus, volume)
+	if save_settings:
+		save_audio_settings()
+
+func get_category_volume(category: StringName) -> float:
+	var bus := _category_to_bus(category)
+	if bus == &"":
+		push_warning("AudioManager could not find volume category '%s'." % String(category))
+		return DEFAULT_BUS_VOLUME
+
+	return get_bus_volume_linear(bus)
+
+func set_master_volume(volume: float, save_settings := true) -> void:
+	set_category_volume(&"master", volume, save_settings)
+
+func get_master_volume() -> float:
+	return get_category_volume(&"master")
+
+func set_music_volume(volume: float, save_settings := true) -> void:
+	set_category_volume(&"music", volume, save_settings)
+
+func get_music_volume() -> float:
+	return get_category_volume(&"music")
+
+func set_sfx_volume(volume: float, save_settings := true) -> void:
+	set_category_volume(&"sfx", volume, save_settings)
+
+func get_sfx_volume() -> float:
+	return get_category_volume(&"sfx")
+
+func set_ui_volume(volume: float, save_settings := true) -> void:
+	set_category_volume(&"ui", volume, save_settings)
+
+func get_ui_volume() -> float:
+	return get_category_volume(&"ui")
+
+func set_ambience_volume(volume: float, save_settings := true) -> void:
+	set_category_volume(&"ambience", volume, save_settings)
+
+func get_ambience_volume() -> float:
+	return get_category_volume(&"ambience")
+
+func set_bus_volume_linear(bus: StringName, volume: float) -> void:
+	var bus_index := AudioServer.get_bus_index(String(bus))
+	if bus_index == -1:
+		push_warning("AudioManager could not find audio bus '%s'." % String(bus))
+		return
+
+	var clamped_volume := clampf(volume, 0.0, 1.0)
+	AudioServer.set_bus_mute(bus_index, clamped_volume <= 0.0)
+	if clamped_volume <= 0.0:
+		AudioServer.set_bus_volume_db(bus_index, MIN_BUS_VOLUME_DB)
+		return
+
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(clamped_volume))
+
+func get_bus_volume_linear(bus: StringName) -> float:
+	var bus_index := AudioServer.get_bus_index(String(bus))
+	if bus_index == -1:
+		push_warning("AudioManager could not find audio bus '%s'." % String(bus))
+		return DEFAULT_BUS_VOLUME
+
+	if AudioServer.is_bus_mute(bus_index):
+		return 0.0
+
+	return clampf(db_to_linear(AudioServer.get_bus_volume_db(bus_index)), 0.0, 1.0)
+
+func save_audio_settings() -> void:
+	var config := ConfigFile.new()
+	for category in get_volume_categories():
+		config.set_value("audio", String(category), get_category_volume(category))
+
+	var error := config.save(AUDIO_SETTINGS_PATH)
+	if error != OK:
+		push_warning("AudioManager could not save audio settings: %s." % error_string(error))
+
+func load_audio_settings() -> void:
+	var config := ConfigFile.new()
+	var error := config.load(AUDIO_SETTINGS_PATH)
+	if error != OK:
+		_apply_default_audio_settings()
+		return
+
+	for category in get_volume_categories():
+		var saved_volume := float(config.get_value("audio", String(category), DEFAULT_BUS_VOLUME))
+		set_category_volume(category, saved_volume, false)
 
 func has_sound(sound_name: StringName) -> bool:
 	return registry != null and registry.has_sound(sound_name)
@@ -198,6 +298,25 @@ func _ensure_runtime_buses() -> void:
 			AudioServer.add_bus(bus_index)
 			AudioServer.set_bus_name(bus_index, String(bus))
 			AudioServer.set_bus_send(bus_index, String(MASTER_BUS))
+
+func _apply_default_audio_settings() -> void:
+	for category in get_volume_categories():
+		set_category_volume(category, DEFAULT_BUS_VOLUME, false)
+
+func _category_to_bus(category: StringName) -> StringName:
+	match category:
+		&"master":
+			return MASTER_BUS
+		&"music":
+			return MUSIC_BUS
+		&"sfx":
+			return SFX_BUS
+		&"ui":
+			return UI_BUS
+		&"ambience":
+			return AMBIENCE_BUS
+		_:
+			return &""
 
 func _on_loop_finished(sound_name: StringName) -> void:
 	if _loop_players.has(sound_name):
